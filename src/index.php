4b15e93a4c0f2cbad60f86d00e0bffcdc4db1721
Pumin Wangprasit <savee2011@hotmail.com>
2024-08-14 14:57:03 +0700

    operation in php

--- src/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$file = fopen('resource.txt', 'r');
-var_dump($file);
-
-?>
---
+++ studied/operation.php
@@ -0,0 +1,10 @@
+<?php
+
+echo 'addition (10 + 5): ' . (10 + 5) . '<br>';
+echo 'subtraction (10 - 5): ' . (10 - 5) . '<br>';
+echo 'multiplication (10 * 5): ' . (10 * 5) . '<br>';
+echo 'division (10 / 5): ' . (10 / 5) . '<br>';
+echo 'modulus (10 % 5): ' . (11 % 5) . '<br>';
+echo 'exponentiation (10 ** 5): ' . (2 ** 2) . '<br>';
+
+?>
